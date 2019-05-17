a5e792cb0feaf220076b23625e07766d9a72c7a7
Mark Hernandez <m.hern1415@gmail.com>
2019-05-17 18:52:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     sorted
   end
 
-  def self.new_from_filename
+  def self.new_from_filename(filename)
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
     song = self.new
